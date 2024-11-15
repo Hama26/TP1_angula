bac533812b746836e2e638b4cbdec6f65505dc00
Hannachi Mohamed <mohamedhannachiest@gmail.com>
2024-11-15 18:29:03 +0100

    Refactor CV component to use observables with async pipe for better performance and error handling

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -1,37 +1,42 @@
-import { Component } from "@angular/core";
-import { Cv } from "../model/cv";
-import { LoggerService } from "../../services/logger.service";
-import { ToastrService } from "ngx-toastr";
-import { CvService } from "../services/cv.service";
+import { Component, OnInit } from '@angular/core';
+import { Cv } from '../model/cv';
+import { CvService } from '../services/cv.service';
+import { ToastrService } from 'ngx-toastr';
+import { LoggerService } from '../../services/logger.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 @Component({
-  selector: "app-cv",
-  templateUrl: "./cv.component.html",
-  styleUrls: ["./cv.component.css"],
+  selector: 'app-cv',
+  templateUrl: './cv.component.html',
+  styleUrls: ['./cv.component.css'],
 })
-export class CvComponent {
-  cvs: Cv[] = [];
-  selectedCv: Cv | null = null;
-  /*   selectedCv: Cv | null = null; */
+export class CvComponent implements OnInit {
+  cvs$!: Observable<Cv[]>; // Observable for async pipe
+  selectedCv$!: Observable<Cv | null>; // Observable for selected CV
   date = new Date();
 
   constructor(
-    private logger: LoggerService,
+    private cvService: CvService,
     private toastr: ToastrService,
-    private cvService: CvService
-  ) {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
+    private logger: LoggerService
+  ) {}
+
+  ngOnInit() {
+    this.logger.logger('je suis le cvComponent');
+    this.toastr.info('Bienvenue dans notre CvTech');
+
+    // Load CVs with error handling
+    this.cvs$ = this.cvService.getCvs().pipe(
+      catchError((error) => {
         this.toastr.error(`
           Attention!! Les données sont fictives, problème avec le serveur.
           Veuillez contacter l'admin.`);
-      },
-    });
-    this.logger.logger("je suis le cvComponent");
-    this.toastr.info("Bienvenu dans notre CvTech");
-    this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+        return of(this.cvService.getFakeCvs());
+      })
+    );
+
+    // Listen for selected CV changes
+    this.selectedCv$ = this.cvService.selectCv$;
   }
 }
