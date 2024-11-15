bf5251c159134b9030c6cb8eaf0dfc71d5fe91c9
Hannachi Mohamed <mohamedhannachiest@gmail.com>
2024-11-15 14:36:31 +0100

    refactor: update component testing modules to use imports instead of declarations; add standalone flag to pipes and components

--- src/app/products/products.component.spec.ts
+++ src/app/products/products.component.spec.ts
@@ -2,14 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
 
+
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ProductsComponent]
-    });
+    imports: [ProductsComponent],
+});
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
